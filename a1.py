15b82dc6cde570245c67655bdd66431dabf193f0
nghidd1110 <memoriesthroughmycams@gmail.com>
2024-09-25 12:38:14 -0400

    change

--- a1.py
+++ a1.py
@@ -1,1 +1,19 @@
-print("hello")
+def calculate_grade(lab,quiz,as_1,as_2,as_3,as_4,mid_1,mid_2,final,prep):
+
+    
+   print("ton totale est: " + (lab/6*20) + (quiz/6*15) +(as_1+as_2+as_3+as_4)*0.04 +(mid_1+mid_2)*0.125 +(final*0.18)+(prep*0.06))
+   
+
+lab=float(input("Enter the number of labs completed: "))
+quiz=float(input("Enter the number of quizzes completed: "))
+as_1=float(input("Enter grade for Assignment 1: "))
+as_2=float(input("Enter grade for Assignment 2: "))
+as_3=float(input("Enter grade for Assignment 3: "))
+as_4=float(input("Enter grade for Assignment 4: "))
+mid_1=float(input("Enter grade for Midterm 1: "))
+mid_2=float(input("Enter grade for Midterm 2: "))
+final=float(input("Enter grade for Final Exam: "))
+prep=float(input("Enter grade for Midterms and Final Preparation: "))
+
+calculate_grade(lab,quiz,as_1,as_2,as_3,as_4,mid_1,mid_2,final,prep)
+
